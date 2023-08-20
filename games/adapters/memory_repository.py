a8e6f65eb94f358518680aa0db49ff290c38a89f
greenaliendev <mryo272@aucklanduni.ac.nz>
2023-08-20 13:33:54 +1200

    added bisect module and how it can be used in memory_repository.py (from lecture 12)

--- games/adapters/memory_repository.py
+++ games/adapters/memory_repository.py
@@ -3,6 +3,7 @@ from typing import List
 from games.adapters.datareader.csvdatareader import GameFileCSVReader
 from games.adapters.repository import AbstractRepository
 from games.domainmodel.model import Game
+from bisect import insort_left
 
 class MemoryRepository(AbstractRepository):
     def __init__(self):
@@ -11,6 +12,7 @@ class MemoryRepository(AbstractRepository):
     def add_game(self, game: Game):
         if isinstance(game, Game):
             self.__games.append(game)
+            #insort_left(self.__games, game)
 
     def get_games(self) -> List[Game]:
         return self.__games
