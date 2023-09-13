99abe67c25c21fea98da4b8622333a2cb2ca48a0
lukitalemon <lukeharrison005@gmail.com>
2023-09-13 17:45:00 +1200

    Attempted to get comments to show
    Try to use course content to do the assignment challenge (impossible)

--- games/gameDescription/gameDescription.py
+++ games/gameDescription/gameDescription.py
@@ -17,6 +17,13 @@ gameDescription_blueprint = Blueprint('gameDescription_bp', __name__)
 def gameDescription(game_id):
     game_details = services.get_game(repo.repo_instance, game_id)
 
+    game_to_show_comments = request.args.get('view_comments_for')
+
+    if game_to_show_comments is None:
+        game_to_show_comments = -1
+    else:
+        game_to_show_comments = int(game_to_show_comments)
+
     if game_details is None:
         # Handle the case where the game is not found
         return render_template('game_not_found.html')
@@ -26,62 +33,63 @@ def gameDescription(game_id):
     return render_template(
         'gameDescription.html',
         game=game_details,
-        form = form 
+        form = form,
+        show_comments_for_game=game_to_show_comments
     )
 
 
 @gameDescription_blueprint.route('/comment', methods=['GET', 'POST'])
-@login_required
-def comment_on_game():
+def review():
     # Obtain the user name of the currently logged in user.
     user_name = session['user_name']
 
-    # Create form. The form maintains state, e.g. when this method is called with a HTTP GET request and populates
+    # Create form. The form maintains state, e.g., when this method is called with a HTTP GET request and populates
     # the form with an article id, when subsequently called with a HTTP POST request, the article id remains in the
     # form.
     form = CommentForm()
 
     if form.validate_on_submit():
-        # Successful POST, i.e. the comment text has passed data validation.
-        # Extract the article id, representing the commented article, from the form.
+        # Successful POST, i.e., the comment text has passed data validation.
+        # Extract the game_id, representing the commented game, from the form.
         game_id = int(form.game_id.data)
 
         # Use the service layer to store the new comment.
         services.add_comment(game_id, form.comment.data, user_name, repo.repo_instance)
 
-        # Retrieve the article in dict form.
+        # Retrieve the game in dict form.
         game = services.get_game(game_id, repo.repo_instance)
+
+        # Retrieve the comments for the game.
         comments = services.get_comments_for_game(game_id, repo.repo_instance)
-        return redirect(url_for('gameDescription_bp.gameDescription', game_id=game_id))
-        # Cause the web browser to display the page of all articles that have the same date as the commented article,
-        # and display all comments, including the new comment.
-        # return redirect(url_for('news_bp.articles_by_date', date=article['date'], view_comments_for=game_id))
+
+        # Cause the web browser to display the page of the commented game and display all comments, including the new comment.
+        return redirect(url_for('gameDescription_bp.review', game=game_id,view_comments_for=game_id))
 
     if request.method == 'GET':
         # Request is a HTTP GET to display the form.
-        # Extract the article id, representing the article to comment, from a query parameter of the GET request.
+        # Extract the game_id, representing the game to comment, from a query parameter of the GET request.
         game_id = int(request.args.get('game'))
 
-        # Store the article id in the form.
+        # Store the game_id in the form.
         form.game_id.data = game_id
     else:
         # Request is a HTTP POST where form validation has failed.
-        # Extract the article id of the article being commented from the form.
+        # Extract the game_id of the game being commented from the form.
         game_id = int(form.game_id.data)
 
-    # For a GET or an unsuccessful POST, retrieve the article to comment in dict form, and return a Web page that allows
+    # For a GET or an unsuccessful POST, retrieve the game to comment in dict form and return a Web page that allows
     # the user to enter a comment. The generated Web page includes a form object.
     game = services.get_game(game_id, repo.repo_instance)
 
-
     return render_template(
-    'gameDescription.html',
-    game=game,
-    form=form,
-    handler_url=url_for('gameDescription_bp.comment_on_game'),
-    game_id=game_id,
-    comments=comments
+        'review.html',
+        game=game,
+        form=form,
+        handler_url=url_for('gameDescription_bp.review'),
+        game_id=game_id,
+        comments=comments  # Add comments to the template context
     )
+
 
 class ProfanityFree:
     def __init__(self, message=None):
--- games/gameDescription/services.py
+++ games/gameDescription/services.py
@@ -1,5 +1,6 @@
 from games.adapters.repository import AbstractRepository
 from games.domainmodel.model import Game, Review 
+from typing import List, Iterable
 
 
 
@@ -41,28 +42,33 @@ def add_comment(game_id: int, comment_text: str, user_name: str, repo: AbstractR
     if user is None:
         raise UnknownUserException
 
-    # Create comment.
-    comment = comment(comment_text)
+    # Create a Review object with the provided data.
+    review = Review(user, game, rating=0, comment=comment_text)
+
+    # Add the review to the game's comments.
+    game.add_review(review)
 
     # Update the repository.
-    repo.add_comment(comment)
+    repo.add_review(review)
 
 
-def get_comments_for_game(game_id, repo: AbstractRepository):
+def get_reviews_for_game(game_id, repo: AbstractRepository):
     game = repo.get_game(game_id)
 
     if game is None:
         raise NonExistentGameException
 
-    return comment_to_dict(game.comments)
+    return reviews_to_dict(game.comments)
 
 
-def comment_to_dict(comment: Review):
-    comment_dict = {
-        'user_name': comment.user.user_name,
-        'game_id': comment.game.id,
-        'comment_text': comment.comment,
-        'timestamp': comment.timestamp
+def review_to_dict_single(review: Review):
+    review_dict = {
+        'user_name': review.user.user_name,
+        'game_id': review.game.game_id,
+        'rating' : review.rating,
+        'comment_text': review.comment
     }
-    return comment_dict
+    return review_dict
 
+def reviews_to_dict(reviews: Iterable[Review]):
+    return [review_to_dict_single(review) for review in reviews]
