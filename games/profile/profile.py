13769dd20d0911979d8a5a494c86de623930a6af
lukitalemon <lukeharrison005@gmail.com>
2023-09-20 11:44:18 +1200

    Touch ups
    Redirects "profile" to login page when user not logged in
    
    Decorated Wishlist buttons
    
    Minor change on rating.
    
    Removed white space stuff on browse and game description.
    
    removed print statements from code.

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -1,12 +1,20 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 import games.adapters.repository as repo # Import MemoryRepository
 from games.profile import services
+from games.authentication.authentication import login_required
+
 
 profile_blueprint = Blueprint('profile_bp', __name__)
 
+
 @profile_blueprint.route('/profile')
+@login_required
 def profile():
     username = session.get('user_name')
+
+    user = repo.repo_instance.get_user(session['user_name'])
+    if user is None:
+        return redirect(url_for('authentication_bp.login'))
 
     user_comments = services.get_comments_by_user(username, repo.repo_instance)
     # You can retrieve the user's wishlist using the MemoryRepository instance
