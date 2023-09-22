3d22844a86393fc70f87a5f53c3cf10455996fac
AsianHard123 <esio569@aucklanduni.ac.nz>
2023-09-22 16:23:39 +1200

    uhhh made lil update on average rating and progresso n test cases

--- tests/unit/test_service.py
+++ tests/unit/test_service.py
@@ -2,8 +2,10 @@ import pytest
 import os
 from games.adapters.repository import AbstractRepository
 from games.domainmodel.model import Publisher, Genre, Game, Review, User, Wishlist
+from games.gameDescription.services import get_game
 from games.browse.services import get_games, get_num_games, get_games_by_genre, get_all_genres, search_games
 from games.browse import services
+from games.gameDescription import services
 from games.adapters import repository as repo
 from games.adapters.memory_repository import MemoryRepository
 
@@ -104,3 +106,61 @@ def test_get_games_if_empty(in_memory_repo):
 def test_get_all_genres_if_empty(in_memory_repo):
     genres = services.get_all_genres(in_memory_repo)
     assert genres == []
+
+def test_get_game(in_memory_repo):
+    game1 = Game(1, "Soccer Game 1")
+    game2 = Game(2, "Racing Game")
+    game3 = Game(3, "Action Adventure")
+
+    publisher1 = Publisher("publisher1")
+    publisher2 = Publisher("publisher2")
+    publisher3 = Publisher("publisher3")
+
+    game1.publisher = publisher1
+    game2.publisher = publisher2
+    game3.publisher = publisher3
+
+    in_memory_repo.add_game(game1)
+    in_memory_repo.add_game(game2)
+    in_memory_repo.add_game(game3)
+
+    expected_games = [
+        {
+            'game_id': 1,
+            'title': 'Soccer Game 1',
+            'release_date': None,
+            'image_url': None,
+            'price' : None,
+            'publisher' : publisher1,
+            'description' : None,
+            'reviews' : None
+        },
+        {
+            'game_id': 2,
+            'title': 'Racing Game',
+            'release_date': None,
+            'image_url': None,
+            'price' : None,
+            'publisher' : publisher2,
+            'description' : None,
+            'reviews' : None
+        },
+        {
+            'game_id': 3,
+            'title': 'Action Adventure',
+            'release_date': None,
+            'image_url': None,
+            'price' : None,
+            'publisher' : publisher3,
+            'description' : None,
+            'reviews' : None
+        }
+    ]
+
+    games = services.get_game(in_memory_repo, 2)
+    assert games == expected_games
+
+
+
+
+
